3a451c4bc5a26f348b8f9adf1f67d9f00d547c98
Jeroen Gremmen <jeroen.gremmen@sayayi.de>
2024-11-14 09:39:22 +0100

    add module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module de.sayayi.lib.antlr {
+
+  requires org.antlr.antlr4.runtime;
+
+  requires static org.jetbrains.annotations;
+
+  exports de.sayayi.lib.antlr4;
+  exports de.sayayi.lib.antlr4.walker;
+  exports de.sayayi.lib.antlr4.syntax;
+
+}
